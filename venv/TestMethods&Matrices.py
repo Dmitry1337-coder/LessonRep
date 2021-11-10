a0723f4199890f1c957123664e94b4de5c7ecdaf
Dmitry1337-coder <86625156+Dmitry1337-coder@users.noreply.github.com>
2021-11-10 20:35:25 +0300

    Update TestMethods&Matrices.py

--- venv/TestMethods&Matrices.py
+++ venv/TestMethods&Matrices.py
@@ -3,16 +3,31 @@ from main import Matrices
 
 matrix1 = Matrices(3, 3)
 matrix1.writeMatrix()
-matrix2 = Matrices(3, 3)
+matrix2 = Matrices(3, 4)
 matrix2.writeMatrix()
 print("Матрица №1:")
 matrix1.ShowMatrix()
 print("Матрица №2:")
 matrix2.ShowMatrix()
-constant = 6
 print("\nМетод умножения функции на число:")
-matrix1.MultiplyMatricesOnConstant(constant)
+matrix1.mulOnConstant()
 print("\nМетод сложения матриц:")
-matrix2.additionMatrices(matrix1)
+matrix2+matrix1
 print("\nМетод разности матриц:")
-matrix2.differenceMatrices(matrix1)
+matrix2-matrix1
+print("\nМетод умножения матриц друг на друга:")
+matrix2*matrix1
+print("\nМетод умножения с поменянными местами матрицами:")
+matrix1*matrix2
+print("\nМетод возведения матрицы в степень:")
+matrix1.ExponentiationMatrix()
+print("\nМетод транспонирования для матрицы 1:")
+matrix1.TranspositionMatrix()
+print("\nМетод транспонирования для матрицы 2:")
+matrix2.TranspositionMatrix()
+print("Проверка на изменение начальных матриц:")
+print("Матрица №1:")
+matrix1.ShowMatrix()
+print("Матрица №2:")
+matrix2.ShowMatrix()
+print("Начальные матрицы остались неизмененными после выполнения действий с ними.")
